6b4fc4ea62458d1feb70a9e5a1c60c9d9cea50db
Jesus Pacheco <jemanuelpachecoo@gmail.com>
2023-08-30 23:48:50 -0300

    remove blog

--- plataforma/docusaurus.config.js
+++ plataforma/docusaurus.config.js
@@ -74,9 +74,9 @@ const config = {
             type: 'docSidebar',
             sidebarId: 'tutorialSidebar',
             position: 'left',
-            label: 'Tutorial',
+            label: 'Plataforma',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          // {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
@@ -116,10 +116,10 @@ const config = {
           {
             title: 'More',
             items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
+              // {
+              //   label: 'Blog',
+              //   to: '/blog',
+              // },
               // {
               //   label: 'GitHub',
               //   href: 'https://github.com/facebook/docusaurus',
